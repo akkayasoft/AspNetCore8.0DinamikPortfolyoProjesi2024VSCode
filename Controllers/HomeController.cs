f6225f95fa60792315c95ceaefb8eafe051d69e9
akkayasoft <akkayasoft@gmail.com>
2024-06-21 07:12:26 +0300

    proje vs code üzerinde oluştu

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCore8._0DinamikPortfolyoProjesi2024VSCode.Models;
+
+namespace AspNetCore8._0DinamikPortfolyoProjesi2024VSCode.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
